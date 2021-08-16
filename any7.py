ac52fe2d9f6e381197bb9113eb6f0ea98fa7f585
Tracey <ttrepper@yahoo.com>
2021-08-15 20:45:56 -0400

    python_syntax

---
+++ any7.py
@@ -0,0 +1,16 @@
+def any7(nums):
+    """Are any of these numbers a 7? (True/False)"""
+
+    # YOUR CODE HERE 
+#if nums == 7:
+ #   print ("should be true")
+#elif nums != 7:
+ #       print ("should be false")
+    for num in nums:
+        if num == 7:
+            return True
+
+    return False
+print("should be true", any7([1, 2, 7, 4, 5]))
+print("should be false", any7([1, 2, 4, 5]))
+
